c1641da974ba6f49fc80b49d6f61ef1010bf05d4
Jon Andreas Bull Larssen <jona.larss@gmail.com>
2022-06-03 17:59:49 +0200

    Add project files

---
+++ script.js
@@ -0,0 +1,47 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var btn = document.getElementById("btn");
+
+function setGradient() {
+	body.style.background = 
+        "linear-gradient(to right, " 
+        + color1.value 
+        + ", " 
+        + color2.value 
+        + ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+function generateRandomColor() {
+    var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    return randomColor;
+};
+
+function randomGradient() {
+	body.style.background = 
+        "linear-gradient(to right, " 
+        + generateRandomColor() 
+        + ", " 
+        + generateRandomColor()
+        + ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+setGradient();
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+btn.addEventListener("click", randomGradient);
+
+
+/* 
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+};
+*/
